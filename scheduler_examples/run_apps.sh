d5f792ead740d2be0a37c28686e23a7df901799a
Marco Fernandes <marco.ac.fernandes2006@gmail.com>
2025-10-01 10:48:37 +0100

    teste commit

--- scheduler_examples/run_apps.sh
+++ scheduler_examples/run_apps.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-# This script runs multiple applications in the background.
-# It waits for all of them to finish before exiting.
 ./app outlook 10 &
 ./app firefox 15 &
 ./app powerpoint 20 &
